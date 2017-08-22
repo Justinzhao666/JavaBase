fa45d05204c8d914bad28e42d5b2554c7b61b267
Justinzhao666 <zhrlova@hotmail.com>
2017-08-22 17:26:37 +0800

    commit in 2017-8-22

---
+++ 异常/src/Template.java
@@ -0,0 +1,15 @@
+/* 
+ * Copyright (c) 2017/8/22 by zhaohaoren
+ * Welcome to visit my site:
+ *  
+ *  www.zhaohaoren.top
+ *
+ * @ Project: JavaCode
+ * @ Lan: Java
+ * @ author zhaohaoren
+ * @ version 1.0 
+ * Template Create on 2017/8/22
+ */
+
+public class Template {
+}
